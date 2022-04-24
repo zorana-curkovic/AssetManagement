ff25b866573ab7fc33b9c7a75cb287a1e946a609
Zorana Curkovic <zorana.curkovic@unity3d.com>
2022-04-24 17:17:13 +0100

    Added Sprite and Atlased Sprite Load examples

---
+++ Assets/AddressableAtlasedSpriteLoader.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.U2D;
+
+public class AddressableAtlasedSpriteLoader : MonoBehaviour
+{
+    public AssetReferenceAtlasedSprite newSpriteAtlas;
+    public string newSpriteAtlasAddress;
+    public string atlasedSpriteName;
+    public bool useAtlasedSpriteName;
+    public bool useManualSpriteLoad;
+    
+    private SpriteRenderer spriteRenderer;
+    
+
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+
+        if (useManualSpriteLoad)
+        {
+            Addressables.LoadAssetAsync<SpriteAtlas>(newSpriteAtlasAddress).Completed += SpriteAtlasLoaded;
+        }
+        else
+        {
+            if (useAtlasedSpriteName)
+            {
+                string atlasedSpriteAddress = newSpriteAtlasAddress + '[' + atlasedSpriteName + ']';
+                Addressables.LoadAssetAsync<Sprite>(atlasedSpriteAddress).Completed += SpriteLoaded;
+            }
+            else
+            {
+                newSpriteAtlas.LoadAssetAsync().Completed += SpriteLoaded;
+
+            }
+        }
+       
+    }
+    
+    void SpriteLoaded(AsyncOperationHandle<Sprite> obj)
+    {
+        switch (obj.Status)
+        {
+            case AsyncOperationStatus.Failed :
+                Debug.LogError("Sprite Loading Failed");
+                break;
+            case AsyncOperationStatus.Succeeded :
+                spriteRenderer.sprite = obj.Result;
+                break;
+            default:
+                break;
+        }
+    }
+
+    void SpriteAtlasLoaded(AsyncOperationHandle<SpriteAtlas> obj)
+    {
+        switch (obj.Status)
+        {
+            case AsyncOperationStatus.Failed :
+                Debug.LogError("Sprite Atlas load failed.");
+                break;
+            case AsyncOperationStatus.Succeeded :
+                spriteRenderer.sprite = obj.Result.GetSprite(atlasedSpriteName);
+                break;
+            default:
+                break;
+        }
+    }
+
+ 
+}
---
+++ Assets/AddressableSpriteLoader.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.U2D;
+
+public class AddressableSpriteLoader : MonoBehaviour
+{
+    public AssetReferenceSprite newSprite;
+    public string newSpriteAddress;
+    public bool useAddress;
+    
+    private SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+
+        if (useAddress)
+            Addressables.LoadAssetAsync<Sprite>(newSpriteAddress).Completed += SpriteLoaded;
+        else
+            newSprite.LoadAssetAsync().Completed += SpriteLoaded;
+    }
+
+    void SpriteLoaded(AsyncOperationHandle<Sprite> obj)
+    {
+        switch (obj.Status)
+        {
+            case AsyncOperationStatus.Failed :
+                Debug.LogError("Sprite Loading Failed");
+                break;
+            case AsyncOperationStatus.Succeeded :
+                spriteRenderer.sprite = obj.Result;
+                break;
+        }
+    }
+}
